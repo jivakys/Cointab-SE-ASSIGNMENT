33fc2cab939e8dcd74e802c0fbded50893b4b843
Jivak Sute <jivakysute@gmail.com>
2024-02-25 16:41:25 +0530

    swagger api documentation added

--- Backend/index.js
+++ Backend/index.js
@@ -23,6 +23,37 @@ sequelize
   });
 
 const PORT = process.env.PORT || 3000;
+
+//Swagger Documentation
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Cointab SE Assignment API's",
+      version: "1.0.0",
+      description: "About :- Documentation of Cointab SE Assignment",
+      license: {
+        name: "Cointab SE Assignment",
+      },
+      contact: {
+        name: "Cointab SE Assignment",
+        url: "https://65d9342bb91d7c7d80a1dd62--venerable-palmier-a34d49.netlify.app/",
+        email: "cointab@gmail.com",
+      },
+    },
+    servers: [
+      {
+        url: `http://localhost:${process.env.PORT}`,
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerSpec = swaggerJSDoc(options);
+app.use("/apidoc", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ Backend/routes/swagger.js
@@ -0,0 +1,186 @@
+/**
+ * @swagger
+ * components:
+ *   schema:
+ *     User:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - phone
+ *         - website
+ *         - city
+ *         - company
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: username of the client or trainer
+ *         email:
+ *           type: string
+ *           description: The user email
+ *         phone:
+ *           type: string
+ *           description: The user phone
+ *         website:
+ *           type: number
+ *           description: The user website
+ *         city:
+ *           type: string
+ *           description: The user city
+ *         company:
+ *           type: string
+ *           description: The user company
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schema:
+ *     Post:
+ *       type: object
+ *       required:
+ *         - userId
+ *         - title
+ *         - body
+ *       properties:
+ *         userId:
+ *           type: number
+ *           description: The userId of the post.
+ *         title:
+ *           type: string
+ *           description: Post Heading
+ *         body:
+ *           type: string
+ *           description: Information of that post.
+ */
+
+/**
+ * @swagger
+ * /users/fetchUsers:
+ *   get:
+ *       summary: To retrieve information about all the users who are registered in the database.
+ *       tags: [User]
+ *       responses:
+ *           200:
+ *               description: All user data has been successfully retrieved.
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           $ref: '#/components/schema/User'
+ *           400:
+ *               description: Something went wrong to fetch User Data
+ */
+
+/**
+ * @swagger
+ * /users//fetchUserById/:id:
+ *   get:
+ *       summary: To retrieve information about one perticular users who are in the database.
+ *       tags: [Users]
+ *       responses:
+ *           200:
+ *               description: Single user data has been successfully retrieved.
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           $ref: '#/components/schema/User'
+ *           400:
+ *               description: Something went wrong to fetch User Data
+ */
+
+/**
+ * @swagger
+ * /users/fetchUserByEmail/:email:
+ *   get:
+ *       summary: To retrieve information about the users who are registered in the database using email.
+ *       tags: [User]
+ *       responses:
+ *           200:
+ *               description: All user data has been successfully retrieved.
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           $ref: '#/components/schema/User'
+ *           400:
+ *               description: Something went wrong to fetch User Data
+ */
+
+/**
+ * @swagger
+ * /users/addUser:
+ *   post:
+ *       summary: To add user data in database.
+ *       tags: [User]
+ *       requestBody:
+ *           required: true
+ *           content:
+ *               application/json:
+ *                   schema:
+ *                       $ref: '#/components/schema/User'
+ *       responses:
+ *           200:
+ *               description: User Data Added in Database Successfully.
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           $ref: '#/components/schema/User'
+ *           400:
+ *               description: Some server error
+ */
+
+/**
+ * @swagger
+ * /posts/fetchPosts/:userId:
+ *   get:
+ *       summary: To retrieve information about all posts of one perticular users.
+ *       tags: [Post]
+ *       responses:
+ *           200:
+ *               description: User all posts data fetch.
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           $ref: '#/components/schema/Post'
+ *           400:
+ *               description: Something went wrong to fetch Post Data
+ */
+
+/**
+ * @swagger
+ * /posts/downloadPostsInExcel/:userId:
+ *   get:
+ *       summary: Download all post data in the excel format.
+ *       tags: [Post]
+ *       responses:
+ *           200:
+ *               description: User all posts data in excel.
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           $ref: '#/components/schema/Post'
+ *           400:
+ *               description: Something went wrong to fetch Post Data
+ */
+
+/**
+ * @swagger
+ * /posts/bulkAddPosts/:userId:
+ *   post:
+ *       summary: Store all the posts of that users into the database..
+ *       tags: [Post]
+ *       requestBody:
+ *           required: true
+ *           content:
+ *               application/json:
+ *                   schema:
+ *                       $ref: '#/components/schema/Post'
+ *       responses:
+ *           200:
+ *               description: Post Data Added in Database Successfully.
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           $ref: '#/components/schema/Post'
+ *           400:
+ *               description: Some server error
+ */
